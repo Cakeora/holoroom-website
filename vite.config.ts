0d356c579348e853795476727d0ba2fe080fbca4
Adham Hazem <adhamcake@gmail.cpm>
2024-12-05 17:10:52 +0200

    index config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
